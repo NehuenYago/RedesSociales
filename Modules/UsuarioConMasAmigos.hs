981e3d95d257b4b0395f47ca68712eb2d38a5a2d
Tadeo Vardánega <tadeo.vardanega@gmail.com>
2023-05-05 21:54:00 -0300

    agrego el caso de que la red social este vacia

--- Modules/UsuarioConMasAmigos.hs
+++ Modules/UsuarioConMasAmigos.hs
@@ -3,16 +3,22 @@ import Modules.FuncionesBase
 import Modules.CantidadDeAmigos
 
 usuarioConMasAmigos :: RedSocial -> Usuario
+usuarioConMasAmigos ([], _, _) = error "La red social no tiene usuarios"
 usuarioConMasAmigos (u : us, rels, pubs) = maximaCantidadDeAmigos (cantidadDeAmigosPorUsuario (u : us, rels, pubs))
+
 
 -- necesito relacionar cantidad de amigos con usuarios -> lista de tupla (cantidadDeAmigos u, u)
 cantidadDeAmigosPorUsuario :: RedSocial -> [(Int, Usuario)]
+cantidadDeAmigosPorUsuario ([], _, _) = []
 cantidadDeAmigosPorUsuario ([u], rels, pubs) = [(cantidadDeAmigos ([u], rels, pubs) u, u)]
 cantidadDeAmigosPorUsuario (u : us, rels, pubs) = (cantidadDeAmigos (u : us, rels, pubs) u, u) : cantidadDeAmigosPorUsuario (us, rels, pubs)
 
+
 -- tomando la lista de tuplas de arriba busco el elemento con maxima cantidad de amigos y retorno el Usuario correspondiente
 maximaCantidadDeAmigos :: [(Int, Usuario)] -> Usuario
+maximaCantidadDeAmigos [] = error "La lista de tuplas está vacía"
 maximaCantidadDeAmigos [(_, u)] = u
 maximaCantidadDeAmigos ((n1, u1) : (n2, u2) : xs)
     | n1 > n2 = maximaCantidadDeAmigos ((n1, u1) : xs)
     | otherwise = maximaCantidadDeAmigos ((n2, u2) : xs)
+
