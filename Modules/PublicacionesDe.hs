a73980f4fa5ac8a3acc966d075be285109dd310c
NehuenYago <nehuenromero@protonmail.com>
2023-05-09 23:14:07 -0300

    la funcion ya no devuelve tipo [String], ahora devuelve tipo [Publicacion]

--- Modules/PublicacionesDe.hs
+++ Modules/PublicacionesDe.hs
@@ -2,15 +2,15 @@ module Modules.PublicacionesDe where
 import Modules.FuncionesBase
 import Modules.FuncionesAuxiliares
 
-publicacionesDe :: RedSocial -> Usuario -> [String]
+publicacionesDe :: RedSocial -> Usuario -> [Publicacion]
 publicacionesDe red u = eliminarRepetidos (publicacionesDeAux (publicaciones red) u 0)
 
-publicacionesDeAux :: [Publicacion] -> Usuario -> Int -> [String]
+publicacionesDeAux :: [Publicacion] -> Usuario -> Int -> [Publicacion]
 publicacionesDeAux listaPublicaciones u n
  | n >= length listaPublicaciones = []
  | otherwise = tomaPublicacion (listaPublicaciones !! n) listaPublicaciones u n
 
-tomaPublicacion :: Publicacion -> [Publicacion] -> Usuario -> Int -> [String]
-tomaPublicacion (usuario,post,_) listaPublicaciones u n
- | usuario == u = post : publicacionesDeAux listaPublicaciones u (n+1)
+tomaPublicacion :: Publicacion -> [Publicacion] -> Usuario -> Int -> [Publicacion]
+tomaPublicacion (usuario,post,likes) listaPublicaciones u n
+ | usuario == u = (usuario,post,likes) : publicacionesDeAux listaPublicaciones u (n+1)
  | otherwise = publicacionesDeAux listaPublicaciones u (n+1)
