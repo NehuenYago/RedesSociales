b47b4fc0f626b469aeddde3dba20e20f5b024d73
NehuenYago <nehuenromero@protonmail.com>
2023-05-09 22:46:11 -0300

    cambie el git add PublicacionesQueLeGustanA.hs  por la funcion devuelveElemento

--- Modules/PublicacionesQueLeGustanA.hs
+++ Modules/PublicacionesQueLeGustanA.hs
@@ -10,7 +10,7 @@ publicacionesQueLeGustanA red usuario = tomaPublicacion (publicaciones red) usua
 tomaPublicacion :: [Publicacion] -> Usuario -> Int -> [Publicacion]
 tomaPublicacion listaPublicaciones usuario n
  | n >= longitud listaPublicaciones = []
- | otherwise = buscaLikes listaPublicaciones n (listaPublicaciones !! n) usuario
+ | otherwise = buscaLikes listaPublicaciones n (devuelveElemento listaPublicaciones n) usuario
 
 -- recibe una publicacion y compara al usuario de quien quiero saber los "me gusta" con los likes de dicha publicacion
 -- si encuentra que le dio like, lo concatena con el resultado de verificar los likes en la siguiente publicacion
@@ -18,5 +18,5 @@ tomaPublicacion listaPublicaciones usuario n
 buscaLikes :: [Publicacion] -> Int -> Publicacion -> Usuario -> [Publicacion]
 buscaLikes listaPublicaciones n (_, _, []) usuario = tomaPublicacion listaPublicaciones usuario (n+1)
 buscaLikes listaPublicaciones n (u, post, l:ls) usuario
- | l == usuario = (listaPublicaciones !! n) : tomaPublicacion listaPublicaciones usuario (n+1)
+ | l == usuario = (devuelveElemento listaPublicaciones n) : tomaPublicacion listaPublicaciones usuario (n+1)
  | otherwise = buscaLikes listaPublicaciones n (u, post, ls) usuario
