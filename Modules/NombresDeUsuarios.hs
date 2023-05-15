66446a0260838e4299e8d2d790bd12015e4fdb5b
bbazzan <brunobazzan@gmail.com>
2023-05-15 20:34:00 -0300

    arreglo nombresDeUsuarios para que no devuelva repetidos, segun especificacion

--- Modules/NombresDeUsuarios.hs
+++ Modules/NombresDeUsuarios.hs
@@ -1,9 +1,10 @@
 module Modules.NombresDeUsuarios where
 import Modules.FuncionesBase
+import Modules.FuncionesAuxiliares
 
 -- devuelve una lista con los nombres de usuario de los usuarios de una red
 nombresDeUsuarios :: RedSocial -> [String]
-nombresDeUsuarios red = extraeNombresDeUsuario (usuarios red)
+nombresDeUsuarios red = eliminarRepetidos (extraeNombresDeUsuario (usuarios red))
 
 extraeNombresDeUsuario :: [Usuario] -> [String]
 extraeNombresDeUsuario [] = []
