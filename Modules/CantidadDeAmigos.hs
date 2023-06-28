9f6291b64e59998b4cb535b60bab08dfd437e8c5
NehuenYago <nehuenromero@protonmail.com>
2023-06-28 14:56:25 -0300

    borro modulos que usamos para hacer el tp

--- Modules/CantidadDeAmigos.hs
+++
@@ -1,8 +0,0 @@
-module Modules.CantidadDeAmigos where
-import Modules.FuncionesBase
-import Modules.FuncionesAuxiliares
-import Modules.AmigosDe
-
--- devuelve la longitud de la lista de usuarios que son amigos del usuario, por lo que entonces devuelve la cantidad de amigos del usuario
-cantidadDeAmigos :: RedSocial -> Usuario -> Int
-cantidadDeAmigos red u = longitud (amigosDe red u)
